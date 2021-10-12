1e25cd53b31b1a11370cae92b94e959b8781cee8
thisisyongsiang <73749752+thisisyongsiang@users.noreply.github.com>
2021-10-13 02:32:18 +0900

    added opentk window
    Started new Class for gameWindow for opentk.
    Learned to open a window with opentk in program class

--- DataStructures_Learning/Program.cs
+++ DataStructures_Learning/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
 
 namespace DataStructures_Learning
 {
@@ -7,6 +12,20 @@ namespace DataStructures_Learning
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(800, 600),
+                Title = "Hello World Learn OpenTK Window",
+                //this is required to run on macos
+                Flags = ContextFlags.ForwardCompatible
+            };
+
+            // This line creates a new instance, and wraps the instance in a using statement so it's automatically disposed once we've exited the block.
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+
         }
     }
 }
---
+++ DataStructures_Learning/Window.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Windowing.Desktop;
+
+namespace DataStructures_Learning
+{
+    public class Window : GameWindow
+    {
+
+        public Window(GameWindowSettings gameWindowSettings,NativeWindowSettings nativeWindowSettings)
+            :base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+            base.OnUpdateFrame(args);
+        }
+    }
+}
